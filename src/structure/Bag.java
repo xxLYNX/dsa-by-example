47e82a6b409a5730ff684997a9bc72149c65da12
xxLYNX <cullenscout1970@gmail.com>
2025-11-29 15:20:35 -0500

    Start of project

---
+++ src/structure/Bag.java
@@ -0,0 +1,8 @@
+package structure;
+
+public class Bag<Element> implements Iterable<Element> {
+
+    private Node<Element> first; //beginning of bag
+    private int n; //number of elements in bag
+
+}
